5b47b4b20f08783a9a1c01744b62e7521b6cd4ee
Llewellyn Falco <isidore@setgame.com>
2012-07-31 07:16:15 -0700

    Inital Create

---
+++ SampleMvc.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleMvc;
+using SampleMvc.Controllers;
+
+namespace SampleMvc.Tests.Controllers
+{
+	[TestClass]
+	public class HomeControllerTest
+	{
+		
+		[TestMethod]
+		public void About()
+		{
+		
+		}
+	}
+}
---
+++ SampleMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleMvc.MvcApplication" Language="C#" %>
